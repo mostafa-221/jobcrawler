ba3a73d6986dc7440699da73b5e8b666c3ea5729
Jelmer Pijnappel <jelmer.pijnappel@ordina.nl>
2020-03-12 10:21:26 +0100

    Initial commit

---
+++ src/main/java/nl/ordina/jobcrawler/GreetingController.java
@@ -0,0 +1,21 @@
+package nl.ordina.jobcrawler;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @CrossOrigin(origins = "localhost:4200")
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam String name) {
+        return new Greeting(counter.incrementAndGet(), name);
+    }
+
+}
