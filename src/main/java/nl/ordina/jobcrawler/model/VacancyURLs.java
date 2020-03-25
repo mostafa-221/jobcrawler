60b2e08840dc46a379ac9db6909bc110c5c02e76
tkoen93 <timokoen93@gmail.com>
2020-03-25 16:40:13 +0100

    Add new class for retrieving vacancy URLs with various attributes

---
+++ src/main/java/nl/ordina/jobcrawler/model/VacancyURLs.java
@@ -0,0 +1,15 @@
+package nl.ordina.jobcrawler.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class VacancyURLs {
+    private String url;
+    private String hours;
+}
