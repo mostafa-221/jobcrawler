e88d1952efbd280688c2e3f895b8d232a2355b66
tkoen93 <timokoen93@gmail.com>
2020-03-26 15:54:14 +0100

    Merged two functions into one

--- src/main/java/nl/ordina/jobcrawler/service/VacancyStarter.java
+++ src/main/java/nl/ordina/jobcrawler/service/VacancyStarter.java
@@ -1,15 +1,16 @@
 package nl.ordina.jobcrawler.service;
 
+import lombok.extern.slf4j.Slf4j;
 import nl.ordina.jobcrawler.controller.MylerVacancyScraper;
 import nl.ordina.jobcrawler.controller.YachtVacancyScraper;
 import nl.ordina.jobcrawler.model.Vacancy;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import nl.ordina.jobcrawler.service.exception.DuplicateRecordFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -17,10 +18,9 @@ This 'starter' class uses the @Scheduled annotation. Every 15 minutes it execute
 Upon fetching the vacancies it runs a check to verify if the vacancy is already present in the database.
 */
 
+@Slf4j
 @Component
 public class VacancyStarter {
-
-    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private VacancyService vacancyService;
@@ -38,51 +38,26 @@ public class VacancyStarter {
     public void cronJobSch() throws IOException {
         // This function gets executed every 15 minutes
         log.info("CRON Scheduled");
-        scrapeYacht();
-        //Disbale Myler as it does not scrape entire vacancy yet.
-//        scrapeMyler();
-        System.out.println();
+        scrape();
     }
 
-    private void scrapeYacht() throws IOException {
-        System.out.println();
-        log.info("YACHT -- Start scraping");
-        List<Vacancy> yachtVacancies = yachtVacancyScraper.getVacancies();
+    private void scrape() throws IOException {
+        List<Vacancy> allVacancies = yachtVacancyScraper.getVacancies();
+        allVacancies.addAll(mylerVacancyScraper.getVacancies());
         int existVacancy = 0;
         int newVacancy = 0;
-        for(Vacancy yachtVacancy : yachtVacancies) {
-            Vacancy existCheck = vacancyService.doesRecordExist(yachtVacancy.getVacancyURL());
+        for(Vacancy vacancy : allVacancies) {
+            Vacancy existCheck = vacancyService.doesRecordExist(vacancy.getVacancyURL());
             if(existCheck == null) {
-                vacancyService.add(yachtVacancy);
+                vacancyService.add(vacancy);
                 newVacancy++;
             } else {
                 existVacancy++;
             }
         }
-        log.info("YACHT -- " + newVacancy + " new vacancies added.");
-        log.info("YACHT -- " + existVacancy + " existing vacancies found.");
-        log.info("YACHT -- Finished scraping");
-        yachtVacancies.clear(); // Clear List<Vacancy> for next time use
-    }
-
-    private void scrapeMyler() throws IOException {
-        System.out.println();
-        log.info("MYLER -- Start scraping");
-        List<Vacancy> mylerVacancies = mylerVacancyScraper.getVacancies();
-        int existVacancy = 0;
-        int newVacancy = 0;
-        for(Vacancy mylerVacany : mylerVacancies) {
-            Vacancy existCheck = vacancyService.doesRecordExist(mylerVacany.getVacancyURL());
-            if(existCheck == null) {
-                vacancyService.add(mylerVacany);
-                newVacancy++;
-            } else {
-                existVacancy++;
-            }
-        }
-        log.info("MYLER -- " + newVacancy + " new vacancies added.");
-        log.info("MYLER -- " + existVacancy + " existing vacancies found.");
-        log.info("MYLER -- Finished scraping");
-        mylerVacancies.clear(); // Clear List<Vacancy> for next time use
+        log.info(newVacancy + " new vacancies added.");
+        log.info(existVacancy + " existing vacancies found.");
+        log.info("Finished scraping");
+        allVacancies.clear();
     }
 }
