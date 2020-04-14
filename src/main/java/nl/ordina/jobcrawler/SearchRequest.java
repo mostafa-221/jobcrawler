c2c10697326d184f1e9e0b3fb1ea8cd40f62b718
Mostafa Abd Elrehim <mostafa.abd.elrehim@ordina.nl>
2020-04-14 17:33:33 +0200

    added lombok notation to simplify search request, to be tested with frontend

--- src/main/java/nl/ordina/jobcrawler/SearchRequest.java
+++ src/main/java/nl/ordina/jobcrawler/SearchRequest.java
@@ -1,27 +1,14 @@
 package nl.ordina.jobcrawler;
 
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+@Value
+@AllArgsConstructor
 public class SearchRequest {
 
-    private final String location;
-    private final String distance;
-    private final String keywords;
-
-    public SearchRequest(String location, String distance, String keywords) {
-        this.location = location;
-        this.distance = distance;
-        this.keywords = keywords;
-    }
-
-    public String getLocation() {
-        return this.location;
-    }
-
-    public String getDistance() {
-        return this.distance;
-    }
-
-    public String getKeywords() {
-        return this.keywords;
-    }
+    String location;
+    String distance;
+    String keywords;
 
 }
