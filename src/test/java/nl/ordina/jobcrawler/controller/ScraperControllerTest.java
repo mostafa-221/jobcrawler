49b5a1ce006d3983b347b54fd2859f8cb3e2defa
Mostafa Abd Elrehim <mostafa.abd.elrehim@ordina.nl>
2020-06-03 12:49:58 +0200

    seperation of controller tests

--- src/test/java/nl/ordina/jobcrawler/controller/ScraperControllerTest.java
+++ src/test/java/nl/ordina/jobcrawler/controller/ScraperControllerTest.java
@@ -1,5 +1,34 @@
-import static org.junit.jupiter.api.Assertions.*;
+package nl.ordina.jobcrawler.controller;
 
+import nl.ordina.jobcrawler.service.VacancyStarter;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ScraperController.class)
 class ScraperControllerTest {
 
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    private VacancyStarter vacancyStarter;
+
+    /**
+     * Tests the /scrape end point, if it calls the VacancyStarter scrape method
+     */
+    @Test
+    void scrape() throws Exception {
+        mockMvc.perform(put("/scrape"))
+                .andExpect(status().isOk());
+
+        verify(vacancyStarter, times(1)).scrape();
+    }
 }
--- src/test/java/nl/ordina/jobcrawler/controller/SearchRequestControllerTest.java
+++ src/test/java/nl/ordina/jobcrawler/controller/SearchRequestControllerTest.java
@@ -1,5 +1,54 @@
-import static org.junit.jupiter.api.Assertions.*;
+package nl.ordina.jobcrawler.controller;
 
-class SearchRequestControllerTest {
+import nl.ordina.jobcrawler.SearchRequest;
+import nl.ordina.jobcrawler.SearchResult;
+import nl.ordina.jobcrawler.model.Vacancy;
+import nl.ordina.jobcrawler.service.VacancyService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(SearchRequestController.class)
+class SearchRequestControllerTest implements ControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    private VacancyService vacancyService;
+
+    private Vacancy mockVacancy;
+
+    @BeforeEach
+    void setUp() {
+        mockVacancy = ControllerTest.vacancyFactory("vacancy");
+    }
+
+    /**
+     * Tests the /searchrequest end point,
+     * for now all it needs to do is to return a new SearchResult with all vacancies
+     */
+    @Test
+    void searchRequest() throws Exception {
+        Vacancy mockVacancy2 = ControllerTest.vacancyFactory("vacancy2");
+        List<Vacancy> vacancies = Arrays.asList(mockVacancy, mockVacancy2);
+        doReturn(vacancies).when(vacancyService).getAllVacancies();
+
+        mockMvc.perform(post("/searchrequest")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ControllerTest.asJsonString(new SearchRequest())))
+                .andExpect(status().isOk())
+                .andExpect(content().string(ControllerTest.asJsonString(new SearchResult(new SearchRequest(), vacancies))));
+    }
 }
