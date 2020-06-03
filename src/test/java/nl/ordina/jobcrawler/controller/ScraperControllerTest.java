bc54e0813a7153561992b4339de2ac58f9533efd
Mostafa Abd Elrehim <mostafa.abd.elrehim@ordina.nl>
2020-06-03 13:47:14 +0200

    added do nothing behaviour to scrape test

--- src/test/java/nl/ordina/jobcrawler/controller/ScraperControllerTest.java
+++ src/test/java/nl/ordina/jobcrawler/controller/ScraperControllerTest.java
@@ -7,8 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,6 +25,8 @@ class ScraperControllerTest {
      */
     @Test
     void scrape() throws Exception {
+        doNothing().when(vacancyStarter).scrape();
+
         mockMvc.perform(put("/scrape"))
                 .andExpect(status().isOk());
 
