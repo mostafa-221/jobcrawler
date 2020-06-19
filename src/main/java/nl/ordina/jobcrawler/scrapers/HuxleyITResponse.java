0b74d902e5e60532e35375d85c56b4aa61119a41
tkoen93 <timokoen93@gmail.com>
2020-06-19 10:07:44 +0200

    Change @Data into @Getter

--- src/main/java/nl/ordina/jobcrawler/scrapers/HuxleyITResponse.java
+++ src/main/java/nl/ordina/jobcrawler/scrapers/HuxleyITResponse.java
@@ -2,17 +2,13 @@ package nl.ordina.jobcrawler.scrapers;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.Map;
 
-@Data
-@Builder
-@AllArgsConstructor
+@Getter
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class HuxleyITResponse {
