18dcc291ace3bdfe0b00cdf3479c57f65ce39fcc
Mostafa Abd Elrehim <mostafa.abd.elrehim@ordina.nl>
2020-07-15 13:29:16 +0200

    devided comment in two lines

--- src/main/java/nl/ordina/jobcrawler/model/Skill.java
+++ src/main/java/nl/ordina/jobcrawler/model/Skill.java
@@ -29,9 +29,9 @@ public class Skill {
     @Id
     private UUID id;
 
-    // Skill may not be null, must be unique. Define column as text as varchar is limited to 255 characters. Skill can be a long sentence. Prevent hibernate DataException.
+    // Skill may not be null, must be unique. Define column as text as varchar is limited to 255 characters.
+    // Skill can be a long sentence. Prevent hibernate DataException.
     @Column(nullable = false, unique = true, columnDefinition = "TEXT")
-    // Unique set to false for MVP. Set to true in combination with entitymanager if skill column is still needed at some point.
     private String name;
 
     @ManyToMany(mappedBy = "skills")
