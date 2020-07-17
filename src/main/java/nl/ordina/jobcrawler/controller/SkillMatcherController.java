8751edb3e45bb8c2cf8244bf635ff44c4d959276
Mostafa Abd Elrehim <mostafa.abd.elrehim@ordina.nl>
2020-07-17 16:53:30 +0200

    removed response code class

--- src/main/java/nl/ordina/jobcrawler/controller/SkillMatcherController.java
+++ src/main/java/nl/ordina/jobcrawler/controller/SkillMatcherController.java
@@ -3,7 +3,6 @@ package nl.ordina.jobcrawler.controller;
 import lombok.extern.slf4j.Slf4j;
 import nl.ordina.jobcrawler.service.SkillMatcherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,22 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SkillMatcherController {
 
-
     private final SkillMatcherService skillMatcherService;
 
     @Autowired
-    public SkillMatcherController(
-            SkillMatcherService skillMatcherService) {
+    public SkillMatcherController(SkillMatcherService skillMatcherService) {
         this.skillMatcherService = skillMatcherService;
     }
 
-
     // rematch the skills
-    @PutMapping(path = "skillmatcher")
-    public ResponseEntity<ResponseCode> relinkSkills() {
+    @PutMapping(path = "/skillmatcher")
+    public void relinkSkills() {
         log.info("relink skills");
         Thread newThread = new Thread(skillMatcherService::relinkSkills);
         newThread.start();
-        return new ResponseEntity<>(new ResponseCode("OK"), HttpStatus.OK);
     }
 }
