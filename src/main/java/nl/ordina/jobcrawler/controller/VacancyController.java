f941d54b52bae73f65e6b9186eb81da0c080c66f
Mostafa Abd Elrehim <mostafa.abd.elrehim@ordina.nl>
2020-07-17 15:44:54 +0200

    moved search request controller methods to vacancy controller

--- src/main/java/nl/ordina/jobcrawler/controller/VacancyController.java
+++ src/main/java/nl/ordina/jobcrawler/controller/VacancyController.java
@@ -1,9 +1,13 @@
 package nl.ordina.jobcrawler.controller;
 
+import nl.ordina.jobcrawler.SearchResult;
 import nl.ordina.jobcrawler.controller.exception.VacancyNotFoundException;
 import nl.ordina.jobcrawler.model.Vacancy;
 import nl.ordina.jobcrawler.service.VacancyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,11 +17,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @CrossOrigin
@@ -38,8 +46,37 @@ public class VacancyController {
      * @return All vacancies in the database.
      */
     @GetMapping
-    public Iterable<Vacancy> getVacancies() {
-        return vacancyService.findAll();
+    public ResponseEntity<SearchResult> getVacancies(
+            @RequestParam(required = false) String value,
+            @RequestParam(required = false) Set<String> skills,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        try {
+            List<Vacancy> vacancyList = new ArrayList<>();
+            Pageable paging = PageRequest.of(page, size);
+
+            Page<Vacancy> vacancies;
+
+            if (value != null && !value.isBlank())
+                vacancies = vacancyService.findByAnyValue(value, paging);
+            else if(!skills.isEmpty())
+                vacancies = vacancyService.findBySkills(skills, paging);
+            else
+                vacancies = vacancyService.findAll(paging);
+            vacancyList = vacancies.getContent();
+
+            if (vacancyList.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            SearchResult searchResult = new SearchResult();
+            searchResult.setVacancies(vacancyList);
+            searchResult.setCurrentPage(vacancies.getNumber());
+            searchResult.setTotalItems(vacancies.getTotalElements());
+            searchResult.setTotalPages(vacancies.getTotalPages());
+            return new ResponseEntity<>(searchResult, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
