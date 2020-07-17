2d00f398cde0d4ba0d5442a5ec2d0f2c3df86c39
Mostafa Abd Elrehim <mostafa.abd.elrehim@ordina.nl>
2020-07-17 13:28:24 +0200

    added a model assembler

---
+++ src/main/java/nl/ordina/jobcrawler/model/assembler/SkillModelAssembler.java
@@ -0,0 +1,19 @@
+package nl.ordina.jobcrawler.model.assembler;
+
+import nl.ordina.jobcrawler.controller.SkillController;
+import nl.ordina.jobcrawler.model.Skill;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+public class SkillModelAssembler implements RepresentationModelAssembler<Skill, EntityModel<Skill>> {
+    @Override
+    public EntityModel<Skill> toModel(Skill skill) {
+        return EntityModel.of(skill,
+                linkTo(methodOn(SkillController.class).getSkill(skill.getId())).withSelfRel(),
+                linkTo(methodOn(SkillController.class).getSkills()).withRel("skills")
+        );
+    }
+}
