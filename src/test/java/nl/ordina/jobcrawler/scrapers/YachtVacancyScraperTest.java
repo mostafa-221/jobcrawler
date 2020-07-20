e1130fd671aad007893ec7993733ef4179f00a84
tkoen93 <timokoen93@gmail.com>
2020-07-20 12:50:50 +0200

    Avoid wildcard import

--- src/test/java/nl/ordina/jobcrawler/scrapers/YachtVacancyScraperTest.java
+++ src/test/java/nl/ordina/jobcrawler/scrapers/YachtVacancyScraperTest.java
@@ -21,7 +21,11 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
